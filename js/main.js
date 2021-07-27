62ebfd7feed502b7dfeb7971f4a4828e7e701ea5
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-27 16:07:51 +0200

    stampa riga di testo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('js ok');
+/*Inizializzare Vue e stampare a schermo un messaggio all’interno di un h1, utilizzando le properietà di Vue presenti in data.
+Bonus
+Aggiungere alla pagina un’immagine, la cui sorgente derivi anch'essa dai data di Vue.
+ */
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        message: 'Hello world',
+    }
+});
+
